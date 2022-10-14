be79cee3b82ac9e40b42fd4a865a8ca560fa3d5c
Maksim <polonets.maksim@gmail.com>
2022-10-14 16:37:58 +0300

    feat: complete vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction) {
+    this.direction = direction;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(string, key) {
+    if (typeof key != 'string' || typeof string != 'string' || key === '' || string === '') throw new Error('Incorrect arguments!')
+    let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    string = string.toUpperCase().split('');
+    let keyIterator = 0;
+    let cipherText = [];
+    let mod = 26;
+    let regex = /[A-Z]/i;
+    key = key.toUpperCase();
+    for (let i = 0; i < string.length; i++) {
+      if (!regex.test(string[i])) {
+        cipherText.push(string[i]);
+        continue;
+      }
+      cipherText.push(letters[((letters.indexOf(string[i]) + letters.indexOf(key[keyIterator]))) % mod])
+
+      if (keyIterator < key.length - 1) keyIterator++
+      else keyIterator = 0;
+    }
+    console.log(cipherText)
+
+    return (this.direction === false) ? cipherText.reverse().join('') : cipherText.join('');
+  }
+
+  decrypt(string, key) {
+    if (typeof key != 'string' || typeof string != 'string' || key === '' || string === '') throw new Error('Incorrect arguments!')
+    let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    string = string.toUpperCase().split('');
+    let keyIterator = 0;
+    let cipherText = [];
+    let mod = 26;
+    let regex = /[A-Z]/i;
+    key = key.toUpperCase();
+    for (let i = 0; i < string.length; i++) {
+
+      if (!regex.test(string[i])) {
+        cipherText.push(string[i]);
+        continue;
+      }
+      cipherText.push(((letters.indexOf(string[i]) - letters.indexOf(key[keyIterator])) >= 0) ? (letters[((letters.indexOf(string[i]) - letters.indexOf(key[keyIterator]))) % mod]) : (letters[((letters.indexOf(string[i]) - letters.indexOf(key[keyIterator]))) + mod]))
+      if (keyIterator < key.length - 1) keyIterator++
+      else keyIterator = 0;
+    }
+
+
+    return (this.direction === false) ? cipherText.reverse().join('') : cipherText.join('');
   }
 }
 
